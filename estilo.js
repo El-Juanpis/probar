5054d2362719848ea55b2fa6e98961b7842b6379
El-Juanpis <juanpablor629@gmail.com>
2023-06-25 14:47:51 -0500

    primer commit

---
+++ estilo.js
@@ -0,0 +1,25 @@
+const contador = document.getElementById("contar");
+const sumar = document.getElementById("incremento");
+const restar = document.getElementById("decremento");
+const reset = document.getElementById("reset");
+
+let numeri = 0;
+
+sumar.addEventListener("click" , ()=>{
+    numeri++;
+    contador.innerHTML = numeri
+} );
+
+restar.addEventListener("click" , ()=>{
+    if (numeri==0) {}
+        else{
+            numeri--;
+            contador.innerHTML = numeri;
+    }
+} );
+
+
+reset.addEventListener("click" , ()=>{
+    numeri= 0;
+    contador.innerHTML = numeri
+} );
